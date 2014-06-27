3d9647cc8192593cfc7c54e615cb190a5505e2c6
douglasjapiassu <douglas.japiassu@gmail.com>
2014-06-27 17:26:44 -0300

    Testes Spring.

--- fontes/avalia-docente-web/src/main/java/br/ufg/inf/es/mb/TesteMB.java
+++ fontes/avalia-docente-web/src/main/java/br/ufg/inf/es/mb/TesteMB.java
@@ -1,17 +1,57 @@
 package br.ufg.inf.es.mb;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
+
+import br.ufg.inf.es.avaliadocente.model.bean.Docente;
+import br.ufg.inf.es.avaliadocente.model.bean.builder.DocenteBuilder;
+import br.ufg.inf.es.avaliadocente.repository.DocenteRepository;
+
 @ManagedBean
+@Configurable
 public class TesteMB {
 
 	private String value = "Teste projeto com Primefaces";
+	
+	private List<Docente> listaDeDocentes;
+	
+	@Autowired
+	public DocenteRepository docenteRepository;
+	
+	@Autowired
+	private Docente docente;
 
 	public String getValue() {
+		docente = new Docente();
+		docente.setNome("teste");
+		this.value = docente.getNome();
 		return value;
 	}
 
 	public void setValue(String value) {
 		this.value = value;
+	}
+	
+	public List<Docente> getListaDeDocentes() {
+		if (docenteRepository != null) {
+			return docenteRepository.findByNome("Evee");
+		}
+		
+		List<Docente> lista = new ArrayList<Docente>();
+		Docente docente1 = new DocenteBuilder().id(1L).matricula("123456").build();
+		Docente docente2 = new DocenteBuilder().id(1L).matricula("654321").build();
+		lista.add(docente1);
+		lista.add(docente2);
+		
+		return lista;
+	}
+	
+	public void setListaDeDocentes(List<Docente> listaDeDocentes) {
+		this.listaDeDocentes = listaDeDocentes;
 	}
 }
